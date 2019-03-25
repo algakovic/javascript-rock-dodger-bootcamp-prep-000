25517619f40ca504e32d582230ff9eca60120fe8
Aleksandar Gakovic <algakovic@gmail.com>
2019-03-25 13:10:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -78,6 +78,15 @@ function endGame() {
 
 
 function moveDodger(e) {
+  /**  calles preventDefault and stopPropagation for every item in the array that equates to e.which.
+   * const code = e.which
+
+  if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+   * 
+    **/
   if (e.which === LEFT_ARROW){
     moveDodgerLeft();
     e.preventDefault();
